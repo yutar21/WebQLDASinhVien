205dd2536a09fcbf1f2e2b99441f01b729ceaf8f
DucTT <thanhduc111121@gmail.com>
2024-12-13 23:48:37 +0700

    Initial commit with .vs excluded

---
+++ WebQLDASinhVien/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebQLDASinhVien.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
